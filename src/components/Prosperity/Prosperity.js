6263e692e08001969251d5456725050870bf25df
gennhonglin <73550732+gennhonglin@users.noreply.github.com>
2023-07-04 01:10:58 -0700

    Developed the reputation and prosperity section where both displays the parties reputation and prosperity. For prosperity adding and subtracting buttons were added to make it easier to increase or decrease the values by one instead of trying to drag it through the input range bar

--- src/components/Prosperity/Prosperity.js
+++ src/components/Prosperity/Prosperity.js
@@ -1,9 +1,65 @@
 import "./Prosperity.scss";
+import {useEffect, useState} from "react";
 
 
 function Prosperity() {
+
+    const [prosperityPoint, setProsperityPoint] = useState(0);
+    const [prosperityLevel, setProsperityLevel] = useState(1);
+
+    useEffect(() => {
+        if (prosperityPoint < 5) {
+            setProsperityLevel(1);
+        } else if (prosperityPoint >= 5 && prosperityPoint < 10) {
+            setProsperityLevel(2);
+        } else if(prosperityPoint >= 10 && prosperityPoint < 16) {
+            setProsperityLevel(3);
+        } else if(prosperityPoint >= 16 && prosperityPoint < 23) {
+            setProsperityLevel(4);
+        } else if(prosperityPoint >= 23 && prosperityPoint < 31) {
+            setProsperityLevel(5);
+        } else if(prosperityPoint >= 31 && prosperityPoint < 40) {
+            setProsperityLevel(6);
+        } else if(prosperityPoint >= 40 && prosperityPoint< 51) {
+            setProsperityLevel(7);
+        } else if(prosperityPoint >= 51 && prosperityPoint < 65) {
+            setProsperityLevel(8);
+        } else if(prosperityPoint >= 65) {
+            setProsperityLevel(9);
+        }
+    }, [prosperityPoint])
+
+
+    const sliderUpdate = (e) => {
+        setProsperityPoint(e);
+    }
+
+    const minusButton = () => {
+        if(prosperityPoint <= 0) {
+            setProsperityPoint(0);
+        } else {
+            setProsperityPoint(prosperityPoint - 1);
+        }
+    }
+
+    const plusButton = () => {
+        if(prosperityPoint >= 100) {
+            setProsperityPoint(100);
+        } else {
+            setProsperityPoint(prosperityPoint + 1);
+        }
+    }
+
+
     return(
         <section className="prosperity">
+            <h2 className="prosperity__title">Prosperity Level: {prosperityLevel}</h2>
+            <div className="prosperity__input">
+                <button onClick={minusButton}>-</button>
+                <input type="range" min="0" max="100" value={prosperityPoint} onChange={e => {sliderUpdate(e.target.value);}} className="prosperity__input-slider"></input>
+                <button onClick={plusButton}>+</button>
+            </div>
+            <h3>{prosperityPoint}</h3>
         </section>
     )
 }
--- src/components/Reputation/Reputation.js
+++ src/components/Reputation/Reputation.js
@@ -1,9 +1,18 @@
 import "./Reputation.scss";
+import { useState } from "react";
 
 function Reputation() {
+
+    const [sliderValue, setSliderValue] = useState(0);
+
+
     return(
         <section className="reputation">
-            
+            <h2 className="reputation__title">Reputation</h2>
+            <div className="reputation__input">
+                <input type="range" min="-20" max="20" defaultValue={sliderValue} onChange={e => setSliderValue(e.target.value)} className="reputation__input-slider"></input>
+            </div>
+            <h3>{sliderValue}</h3>
         </section>
     )
 }
